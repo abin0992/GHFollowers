bdd64eecf2046b2ee3b75411c28802a29dae1c10
Abin <abin.baby@riskmethods.net>
2021-01-02 01:06:49 +0100

    Adds more tests for search VC

--- GHFollowersTests/ViewController Tests/GFSearchVCTests.swift
+++ GHFollowersTests/ViewController Tests/GFSearchVCTests.swift
@@ -5,28 +5,103 @@
 //  Created by Abin Baby on 01/01/21.
 //
 
+@testable import GHFollowers
 import XCTest
 
 class GFSearchVCTests: XCTestCase {
 
+    // MARK: - Subject under test
+
+    var sut: GFSearchViewController!
+    var navigationController: UINavigationController!
+
+    // MARK: - Setup View controller
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard
+          .instantiateViewController(
+            withIdentifier: GFSearchViewController.className) as? GFSearchViewController
+
+        navigationController = UINavigationController(rootViewController: sut)
+        UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController = navigationController
+
+        sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        navigationController = nil
+
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // MARK: - Tests
+
+    func test_OutletsShouldBeConnected() {
+            XCTAssertNotNil(sut.usernameTextField, "usernameTextField not found")
+            XCTAssertNotNil(sut.getFollowersButton, "Get followers button not found")
+        }
+
+    func test_HasUsernameTextField() {
+        let usernameTextFieldIsSubView: Bool = sut.usernameTextField?.isDescendant(of: sut.view) ?? false
+          XCTAssertTrue(usernameTextFieldIsSubView)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_GetFollwersButtonHasAction() {
+      let getFollowersButton: UIButton = sut.getFollowersButton
+
+      guard let actions = getFollowersButton.actions(forTarget: sut, forControlEvent: .touchUpInside) else {
+        XCTFail()
+        return
+      }
+
+      XCTAssertTrue(actions.contains("getFollowersButtonAction:"))
+    }
+
+    func test_GetFollwersButton_WhenTapped_FollwerListIsPushed() {
+        sut.usernameTextField.text = "testUser"
+        sut.getFollowersButton.sendActions(for: .touchUpInside)
+        RunLoop.current.run(until: Date())
+
+        guard let _ = navigationController.topViewController as? GFFollowerListViewController else {
+            XCTFail("Follower list view controller not pushed")
+            return
         }
     }
 
+    func test_Controller_ShowsAlert_ForEmptyUsername() {
+
+        sut.usernameTextField.text = ""
+            // Tap get follwers button when TextFields have empty state
+        sut.getFollowersButton.sendActions(for: .touchUpInside)
+
+        let alertExpectation: XCTestExpectation = expectation(description: "alert")
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+
+            guard let alertController: GFAlertViewController = self.navigationController.viewControllers.last?.presentedViewController as? GFAlertViewController else {
+                XCTFail("GFAlertViewController was not presented")
+                return
+            }
+
+            XCTAssertNotNil(alertController, "GFAlertViewController was not presented")
+
+            let expectedAlertTitle: String = Alert.emptyUsernameTitle
+            let actualAlertTitle: String = alertController.titleLabel.text ?? "Test Title"
+            XCTAssertEqual(expectedAlertTitle, actualAlertTitle)
+
+            let expectedAlertMessage: String = Alert.emptyUsernameMessage
+            let actualAlertMessage: String = alertController.messageLabel.text ?? "Test Message"
+            XCTAssertEqual(expectedAlertMessage, actualAlertMessage)
+
+            let expectedActionTitle: String = Alert.okButtonLabel
+            let actualActionTitle: String = alertController.actionButton.titleLabel?.text ?? "Test Botton"
+            XCTAssertEqual(expectedActionTitle, actualActionTitle)
+
+            alertExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 2.0, handler: nil)
+        }
 }
